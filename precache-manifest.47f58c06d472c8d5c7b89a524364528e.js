22df18c5533af31525045d5c1ec785dd07f52581
tricksterCodess <tricksterCodess@users.noreply.github.com>
2020-12-04 20:34:03 -0600

    Updates

---
+++ precache-manifest.47f58c06d472c8d5c7b89a524364528e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d171b3df55ffc24c1895044bbfad2fbe",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "179849a6054e7d50e1f9",
+    "url": "/portfolio/static/css/2.d6500151.chunk.css"
+  },
+  {
+    "revision": "ac40ac9167d7009d4c5e",
+    "url": "/portfolio/static/css/main.b85a89bc.chunk.css"
+  },
+  {
+    "revision": "179849a6054e7d50e1f9",
+    "url": "/portfolio/static/js/2.c438414b.chunk.js"
+  },
+  {
+    "revision": "d8d02e0e94d54bd11ab40c5508f05722",
+    "url": "/portfolio/static/js/2.c438414b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac40ac9167d7009d4c5e",
+    "url": "/portfolio/static/js/main.7c363fc8.chunk.js"
+  },
+  {
+    "revision": "ee3d72d161f98a5d4b9d",
+    "url": "/portfolio/static/js/runtime-main.f18b6ef5.js"
+  },
+  {
+    "revision": "795f3af4656ffca68b453286136ebefe",
+    "url": "/portfolio/static/media/background.795f3af4.png"
+  }
+]);
